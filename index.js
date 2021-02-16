6600f8e289ff28f26e351629e6b21de762294106
klee128 <klee128@ucsc.edu>
2021-02-15 21:40:52 -0800

    messge

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
+    console.log('say hay')
     res.send('HEY!')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
